37376765b1af84c43ce0e607d45215b905a8f167
nalek0 <belash.aleksandr777@gmail.com>
2024-10-20 23:35:23 +0300

    App activity layout

---
+++ app/src/main/java/com/nalek0/firebase/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nalek0.firebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
